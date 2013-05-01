f9b8466b2b4ebac2bec64402214d2fde292c789b
Thomas Haukland <thomas.haukland@gmail.com>
2013-05-01 03:14:02 -0400

    missing files

---
+++ server/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoConnectionString: process.env.mongoConnectionString || 
+  'mongodb://hours:hours@dharma.mongohq.com:10007/hours' // 'mongodb://hours:hours@ds047057.mongolab.com:47057/hours'
+};
