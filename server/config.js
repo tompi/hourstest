b024bd9d8cc7154efe77690252387266e8b47c7d
Thomas Haukland <thomas.haukland@gmail.com>
2013-05-03 16:45:23 -0400

    azure mongo

--- server/config.js
+++ server/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoConnectionString: process.env.mongoConnectionString || 'mongodb://hours:hours@dharma.mongohq.com:10007/hours' // 'mongodb://hours:hours@ds047057.mongolab.com:47057/hours'
+  mongoConnectionString: process.env.mongoConnectionString || 'mongodb://hours:hours@widmore.mongohq.com:10000/azurehours' //'mongodb://hours:hours@dharma.mongohq.com:10007/hours' // 'mongodb://hours:hours@ds047057.mongolab.com:47057/hours'
 };
